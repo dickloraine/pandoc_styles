a2f698f96c54c3954f485dd346e275bdb9078e9f
dickloraine <dickloraine@gmx.net>
2018-12-04 16:11:12 +0100

    added tests for some more filter

--- tests/test_filter.py
+++ tests/test_filter.py
@@ -5,9 +5,9 @@ from fixtures import run_script, config_dir  # pylint: disable=W0611
 
 
 @pytest.fixture
-def test_transform_filter(tmpdir, run_script):
-    def _test_filter(filt, args, fmts):
-        test_base = BASE.format(f"./config_dir/filter/{filt}", f"{{{args}}}")
+def test_filter(tmpdir, run_script):
+    def _test_filter(filt, args, fmts, base=TF_BASE):
+        test_base = base.format(f"./config_dir/filter/{filt}", f"{{{args}}}")
         test_file = file_write("test.md", test_base, tmpdir)
 
         for fmt, fmt_txt, fmt_open, fmt_close in fmts:
@@ -19,35 +19,50 @@ def test_transform_filter(tmpdir, run_script):
     return _test_filter
 
 
-def test_alignment(config_dir, test_transform_filter):
-    test_transform_filter('alignment.py', '.align .center', [
-        ('html', HTML_BASE, '<div class="align_center">', '</div>'),
-        ('latex', LATEX_BASE, '\\begin{center}\n', '\n\end{center}')
-    ])
+def test_alignment(config_dir, test_filter):
+    test_filter('alignment.py', '.align .center',
+                [('html', TF_HTML_BASE, '<div class="align_center">', '</div>'),
+                 ('latex', TF_LATEX_BASE, '\\begin{center}\n', '\n\end{center}')])
 
 
-def test_epigraph(config_dir, test_transform_filter):
-    test_transform_filter('epigraph.py', '.epigraph', [
-        ('html', HTML_BASE,
-         '<div class="Epigraph">',
-         '</div>'),
-        ('latex', LATEX_BASE, '\\dictum[]{',
-         '}\n\\par\n\\vspace{\\baselineskip}\n\\par\n\\noindent')
-    ])
-    test_transform_filter('epigraph.py', '.epigraph author="Sherlock Holmes"', [
-        ('html', HTML_BASE,
-         '<div class="Epigraph">',
-         '<p class="EpigraphAuthor">Sherlock Holmes</p>\n</div>'),
-        ('latex', LATEX_BASE, '\\dictum[Sherlock Holmes]{',
-         '}\n\\par\n\\vspace{\\baselineskip}\n\\par\n\\noindent')
-    ])
+def test_epigraph(config_dir, test_filter):
+    test_filter('epigraph.py', '.epigraph',
+                [('html', TF_HTML_BASE, '<div class="Epigraph">', '</div>'),
+                 ('latex', TF_LATEX_BASE, '\\dictum[]{',
+                  '}\n\\par\n\\vspace{\\baselineskip}\n\\par\n\\noindent')])
+    test_filter('epigraph.py', '.epigraph author="Sherlock Holmes"',
+                [('html', TF_HTML_BASE,
+                  '<div class="Epigraph">',
+                  '<p class="EpigraphAuthor">Sherlock Holmes</p>\n</div>'),
+                 ('latex', TF_LATEX_BASE,
+                  '\\dictum[Sherlock Holmes]{',
+                  '}\n\\par\n\\vspace{\\baselineskip}\n\\par\n\\noindent')])
 
 
-def test_noindent(config_dir, test_transform_filter):
-    test_transform_filter('noindent.py', '.noindent', [
-        ('html', HTML_BASE, '<div class="noindent">', '</div>'),
-        ('latex', LATEX_BASE, '\\noindent\n', '')
-    ])
+def test_include(tmpdir, config_dir, test_filter):
+    incl_file = file_write("test_incl.md", "Text to Include", tmpdir)
+    base = BASE.format("{}", f'~~~{{}}\n{incl_file}\n~~~')
+    test_filter('include.py', '.include',
+                [('html', HTML_BASE, '<p>Text to Include</p>', ''),
+                 ('latex', LATEX_BASE, 'Text to Include', '')],
+                base)
+
+
+def test_new_page(config_dir, test_filter):
+    base = BASE.format("{}", '~~~{}\n\n~~~')
+    test_filter('new_page.py', '.new_page',
+                [('html', HTML_BASE, '<div class="pagebreak"></div>', ''),
+                 ('latex', LATEX_BASE, r"\clearpage", '')],
+                base)
+    test_filter('new_page.py', '.new_page .odd-page',
+                [('latex', LATEX_BASE, r"\cleardoublepage", '')],
+                base)
+
+
+def test_noindent(config_dir, test_filter):
+    test_filter('noindent.py', '.noindent',
+                [('html', TF_HTML_BASE, '<div class="noindent">', '</div>'),
+                 ('latex', TF_LATEX_BASE, '\\noindent\n', '')])
 
 
 BASE = """\
@@ -61,17 +76,19 @@ style-definition:
       wrap: none
 ---
 
-The campaign brought honours and promotion to many, but
-for me it had nothing but misfortune and disaster. I was removed from my
-brigade and attached to the Berkshires, with whom I served at the fatal battle
-of Maiwand.
+The campaign brought honours and promotion to many, but for me it had nothing but misfortune and disaster. I was removed from my brigade and attached to the Berkshires, with whom I served at the fatal battle of Maiwand.
 
-There I was struck on the shoulder by a Jezail bullet, which
-shattered the bone and grazed the subclavian artery. I should have
-fallen into the hands of the murderous Ghazis had it not been for the
-devotion and courage shown by Murray, my orderly, who threw me across a
-pack-horse, and succeeded in bringing me safely to the British lines.
+There I was struck on the shoulder by a Jezail bullet, which shattered the bone and grazed the subclavian artery. I should have fallen into the hands of the murderous Ghazis had it not been for the devotion and courage shown by Murray, my orderly, who threw me across a pack-horse, and succeeded in bringing me safely to the British lines.
 
+{}
+
+The campaign brought honours and promotion to many, but for me it had nothing but misfortune and disaster. I was removed from my brigade and attached to the Berkshires, with whom I served at the fatal battle of Maiwand.
+
+There I was struck on the shoulder by a Jezail bullet, which shattered the bone and grazed the subclavian artery. I should have fallen into the hands of the murderous Ghazis had it not been for the devotion and courage shown by Murray, my orderly, who threw me across a pack-horse, and succeeded in bringing me safely to the British lines.
+"""
+
+
+TF_BASE = BASE.format("{}", """\
 ~~~{}
 The campaign brought honours and promotion to many, but
 for me it had nothing but misfortune and disaster. I was removed from my
@@ -84,36 +101,35 @@ fallen into the hands of the murderous Ghazis had it not been for the
 devotion and courage shown by Murray, my orderly, who threw me across a
 pack-horse, and succeeded in bringing me safely to the British lines.
 ~~~
-
-The campaign brought honours and promotion to many, but
-for me it had nothing but misfortune and disaster. I was removed from my
-brigade and attached to the Berkshires, with whom I served at the fatal battle
-of Maiwand.
-
-There I was struck on the shoulder by a Jezail bullet, which
-shattered the bone and grazed the subclavian artery. I should have
-fallen into the hands of the murderous Ghazis had it not been for the
-devotion and courage shown by Murray, my orderly, who threw me across a
-pack-horse, and succeeded in bringing me safely to the British lines.
-
-"""
+""")
 
 HTML_BASE = """\
 <p>The campaign brought honours and promotion to many, but for me it had nothing but misfortune and disaster. I was removed from my brigade and attached to the Berkshires, with whom I served at the fatal battle of Maiwand.</p>
 <p>There I was struck on the shoulder by a Jezail bullet, which shattered the bone and grazed the subclavian artery. I should have fallen into the hands of the murderous Ghazis had it not been for the devotion and courage shown by Murray, my orderly, who threw me across a pack-horse, and succeeded in bringing me safely to the British lines.</p>
-{}
-<p>The campaign brought honours and promotion to many, but for me it had nothing but misfortune and disaster. I was removed from my brigade and attached to the Berkshires, with whom I served at the fatal battle of Maiwand.</p>
-<p>There I was struck on the shoulder by a Jezail bullet, which shattered the bone and grazed the subclavian artery. I should have fallen into the hands of the murderous Ghazis had it not been for the devotion and courage shown by Murray, my orderly, who threw me across a pack-horse, and succeeded in bringing me safely to the British lines.</p>
-{}
+{}{}
 <p>The campaign brought honours and promotion to many, but for me it had nothing but misfortune and disaster. I was removed from my brigade and attached to the Berkshires, with whom I served at the fatal battle of Maiwand.</p>
 <p>There I was struck on the shoulder by a Jezail bullet, which shattered the bone and grazed the subclavian artery. I should have fallen into the hands of the murderous Ghazis had it not been for the devotion and courage shown by Murray, my orderly, who threw me across a pack-horse, and succeeded in bringing me safely to the British lines.</p>
 """
+
+TF_HTML_BASE = HTML_BASE.format("", """\
+{}
+<p>The campaign brought honours and promotion to many, but for me it had nothing but misfortune and disaster. I was removed from my brigade and attached to the Berkshires, with whom I served at the fatal battle of Maiwand.</p>
+<p>There I was struck on the shoulder by a Jezail bullet, which shattered the bone and grazed the subclavian artery. I should have fallen into the hands of the murderous Ghazis had it not been for the devotion and courage shown by Murray, my orderly, who threw me across a pack-horse, and succeeded in bringing me safely to the British lines.</p>
+{}""")
 
 LATEX_BASE = """\
 The campaign brought honours and promotion to many, but for me it had nothing but misfortune and disaster. I was removed from my brigade and attached to the Berkshires, with whom I served at the fatal battle of Maiwand.
 
 There I was struck on the shoulder by a Jezail bullet, which shattered the bone and grazed the subclavian artery. I should have fallen into the hands of the murderous Ghazis had it not been for the devotion and courage shown by Murray, my orderly, who threw me across a pack-horse, and succeeded in bringing me safely to the British lines.
 
+{}{}
+
+The campaign brought honours and promotion to many, but for me it had nothing but misfortune and disaster. I was removed from my brigade and attached to the Berkshires, with whom I served at the fatal battle of Maiwand.
+
+There I was struck on the shoulder by a Jezail bullet, which shattered the bone and grazed the subclavian artery. I should have fallen into the hands of the murderous Ghazis had it not been for the devotion and courage shown by Murray, my orderly, who threw me across a pack-horse, and succeeded in bringing me safely to the British lines.
+"""
+
+TF_LATEX_BASE = LATEX_BASE.format("", """\
 {}The campaign brought honours and promotion to many, but for me it had
 nothing but misfortune and disaster. I was removed from my brigade and
 attached to the Berkshires, with whom I served at the fatal battle of
@@ -123,9 +139,4 @@ There I was struck on the shoulder by a Jezail bullet, which shattered
 the bone and grazed the subclavian artery. I should have fallen into the
 hands of the murderous Ghazis had it not been for the devotion and
 courage shown by Murray, my orderly, who threw me across a pack-horse,
-and succeeded in bringing me safely to the British lines.{}
-
-The campaign brought honours and promotion to many, but for me it had nothing but misfortune and disaster. I was removed from my brigade and attached to the Berkshires, with whom I served at the fatal battle of Maiwand.
-
-There I was struck on the shoulder by a Jezail bullet, which shattered the bone and grazed the subclavian artery. I should have fallen into the hands of the murderous Ghazis had it not been for the devotion and courage shown by Murray, my orderly, who threw me across a pack-horse, and succeeded in bringing me safely to the British lines.
-"""
+and succeeded in bringing me safely to the British lines.{}""")
