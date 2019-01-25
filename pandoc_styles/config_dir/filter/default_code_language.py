000bca35baadade723a7927b8e22637c7a211072
dickloraine <dickloraine@gmx.net>
2019-01-25 10:11:30 +0100

    use get metadata

--- pandoc_styles/config_dir/filter/default_code_language.py
+++ pandoc_styles/config_dir/filter/default_code_language.py
@@ -3,7 +3,7 @@ from pandoc_styles import run_pandoc_styles_filter, CodeBlock
 
 def default_lang(self):
     if not self.classes:
-        lang = self.get_cfg_metadata("default-code-language")
+        lang = self.get_metadata("default-code-language")
         if lang:
             self.classes.append(lang)
 
