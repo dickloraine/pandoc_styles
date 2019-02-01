5f65e0b5ee67b825c1328ae1d639bd16786a84db
dickloraine <dickloraine@gmx.net>
2019-02-01 12:36:18 +0100

    add has extension

--- pandoc_styles/__init__.py
+++ pandoc_styles/__init__.py
@@ -1,5 +1,5 @@
 from .utils import (change_dir, file_read, file_write, make_list, run_process,
-                    expand_directories)
+                    expand_directories, has_extension)
 from .flight_scripts import run_postflight_script, run_preflight_script
 from .filter import *  # pylint: disable=wildcard-import
 from .main import PandocStyles
