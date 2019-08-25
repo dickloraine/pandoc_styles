04ea9a6b5b0099d39494f9665bc39c71b0095306
dickloraine <dickloraine@gmx.net>
2019-08-25 19:01:16 +0200

    updated tests

--- tests/test_filter.py
+++ tests/test_filter.py
@@ -16,44 +16,38 @@ def test_filter(tmpdir, run_script, copy_from_config):
             ps = run_script(f'{test_file} -t {fmt}')
             assert ps
             assert ps.stdout == f"INFO: Build {fmt}\n"
+            print(target)
+            print("-----------------------------------")
+            print(file_read(f"test.{fmt}", tmpdir))
             assert file_read(f"test.{fmt}", tmpdir) == target
     return _test_filter
 
 
 def test_alignment(test_filter):
-    test_filter('alignment.py', '.align .center',
+    test_filter('alignment.py', '.align_center',
                 [('html', TF_HTML_BASE, '<div class="align_center">', '</div>'),
-                 ('latex', TF_LATEX_BASE, '\\begin{center}\n', '\n\\end{center}')])
+                 ('latex', DIV_LATEX_BASE, '\\begin{center}\n', '\n\n\\end{center}')])
 
 
 def test_custom_styles(test_filter):
     test_filter('custom_styles.py', '.custom .teststyle',
                 [('html', TF_HTML_BASE, '<div class="teststyle">', '</div>'),
-                 ('latex', TF_LATEX_BASE, '\\begin{teststyle}\n', '\n\\end{teststyle}')])
-    test_filter('custom_styles.py', '.custom pdf=pdf_teststyle html=html_teststyle',
-                [('html', TF_HTML_BASE, '<div class="html_teststyle">', '</div>'),
-                 ('latex', TF_LATEX_BASE, '\\begin{pdf_teststyle}\n',
-                  '\n\\end{pdf_teststyle}')])
+                 ('latex', DIV_LATEX_BASE, '\\begin{teststyle}\n',
+                  '\n\n\\end{teststyle}')])
 
-def test_div(test_filter):
-    test_filter('divs.py', '.teststyle', [
-        ('html', TF_HTML_BASE, '<div class="teststyle">', '</div>'),
-        ('latex', DIV_LATEX_BASE, '\\begin{teststyle}\n', '\\end{teststyle}')
-        ],
-                DIV_BASE)
 
 def test_epigraph(test_filter):
     test_filter('epigraph.py', '.epigraph',
                 [('html', TF_HTML_BASE, '<div class="Epigraph">', '</div>'),
-                 ('latex', TF_LATEX_BASE, '\\dictum[]{',
-                  '}\n\\par\n\\vspace{\\baselineskip}\n\\par\n\\noindent')])
+                 ('latex', DIV_LATEX_BASE, '\\dictum[]{\n',
+                  '\n\n}\n\n\\par\n\\vspace{\\baselineskip}\n\\par\n\\noindent')])
     test_filter('epigraph.py', '.epigraph author="Sherlock Holmes"',
                 [('html', TF_HTML_BASE,
                   '<div class="Epigraph">',
                   '<p class="EpigraphAuthor">Sherlock Holmes</p>\n</div>'),
-                 ('latex', TF_LATEX_BASE,
-                  '\\dictum[Sherlock Holmes]{',
-                  '}\n\\par\n\\vspace{\\baselineskip}\n\\par\n\\noindent')])
+                 ('latex', DIV_LATEX_BASE,
+                  '\\dictum[Sherlock Holmes]{\n',
+                  '\n\n}\n\n\\par\n\\vspace{\\baselineskip}\n\\par\n\\noindent')])
 
 
 def test_include(tmpdir, test_filter):
@@ -66,20 +60,18 @@ def test_include(tmpdir, test_filter):
 
 
 def test_new_page(test_filter):
-    base = BASE.format("{}", '~~~{}\n\n~~~')
+    # base = BASE.format("{}", '~~~{}\n\n~~~')
     test_filter('new_page.py', '.new_page',
                 [('html', HTML_BASE, '<div class="pagebreak"></div>', ''),
-                 ('latex', LATEX_BASE, r"\clearpage", '')],
-                base)
+                 ('latex', LATEX_BASE, r"\clearpage", '')])
     test_filter('new_page.py', '.new_page .odd-page',
-                [('latex', LATEX_BASE, r"\cleardoublepage", '')],
-                base)
+                [('latex', LATEX_BASE, r"\cleardoublepage", '')])
 
 
 def test_noindent(test_filter):
     test_filter('noindent.py', '.noindent',
                 [('html', TF_HTML_BASE, '<div class="noindent">', '</div>'),
-                 ('latex', TF_LATEX_BASE, '\\noindent\n', '')])
+                 ('latex', DIV_LATEX_BASE, '\\noindent\n', '')])
 
 
 def test_quote(test_filter):
@@ -88,28 +80,28 @@ def test_quote(test_filter):
                   '<div class="QuoteBlock">',
                   '<p class="QuoteScource">Test</p>\n'
                   '<p class="QuoteAuthor">Sher Holm</p>\n</div>'),
-                 ('latex', TF_LATEX_BASE,
+                 ('latex', DIV_LATEX_BASE,
                   "\\begin{quote}\n",
-                  "\n\n\\hspace*{\\fill}\\textbf{Test}\\linebreak"
-                  "\\hspace*{\\fill}\\textit{Sher Holm}\n\\end{quote}")])
+                  "\n\n\\hspace*{\\fill}\\textbf{Test}\n\n"
+                  "\\hspace*{\\fill}\\textit{Sher Holm}\n\n\\end{quote}")])
     test_filter('quote.py', '.quote .one-line author="Sher Holm" title=Test',
                 [('html', TF_HTML_BASE,
                   '<div class="QuoteBlock">',
-                  '<p class="QuoteSingle"><em>Sher Holm</em> &mdash; '
+                  '<p class="QuoteSingle"><em>Sher Holm</em>&mdash;'
                   "<strong>Test</strong></p>\n</div>"),
-                 ('latex', TF_LATEX_BASE,
+                 ('latex', DIV_LATEX_BASE,
                   "\\begin{quote}\n",
-                  "\n\\hfill \\textit{Sher Holm} --- \\textbf{Test}\n\\end{quote}")])
+                  "\n\n\\hfill \\textit{Sher Holm} --- \\textbf{Test}\n\n\\end{quote}")])
     test_filter('quote.py', '.quote .top author="Sher Holm" title=Test',
                 [('html', TF_HTML_BASE,
                   '<div class="QuoteBlock">\n'
                   '<p class="QuoteScourceTop">Test</p>\n'
                   '<p class="QuoteAuthorTop">Sher Holm</p>',
                   "</div>"),
-                 ('latex', TF_LATEX_BASE,
-                  "\\begin{quote}\n\\begin{center}\n\\large\\textbf{Test}\n"
-                  "\n\\normalsize\\textit{Sher Holm}\n\\end{center}\n",
-                  "\n\\end{quote}")])
+                 ('latex', DIV_LATEX_BASE,
+                  "\\begin{quote}\n\n\\begin{center}\n\n\\large\\textbf{Test}\n"
+                  "\n\\normalsize\\textit{Sher Holm}\n\n\\end{center}\n",
+                  "\n\n\\end{quote}")])
 
 
 BASE = """\
@@ -136,6 +128,21 @@ There I was struck on the shoulder by a Jezail bullet, which shattered the bone 
 
 
 TF_BASE = BASE.format("{}", """\
+::: {}
+The campaign brought honours and promotion to many, but
+for me it had nothing but misfortune and disaster. I was removed from my
+brigade and attached to the Berkshires, with whom I served at the fatal battle
+of Maiwand.
+
+There I was struck on the shoulder by a Jezail bullet, which
+shattered the bone and grazed the subclavian artery. I should have
+fallen into the hands of the murderous Ghazis had it not been for the
+devotion and courage shown by Murray, my orderly, who threw me across a
+pack-horse, and succeeded in bringing me safely to the British lines.
+:::
+""")
+
+TF_BASE_CB = BASE.format("{}", """\
 ~~~{}
 The campaign brought honours and promotion to many, but
 for me it had nothing but misfortune and disaster. I was removed from my
@@ -204,7 +211,8 @@ and succeeded in bringing me safely to the British lines.
 """)
 
 DIV_LATEX_BASE = LATEX_BASE.format("", """\
-{}The campaign brought honours and promotion to many, but for me it had nothing but misfortune and disaster. I was removed from my brigade and attached to the Berkshires, with whom I served at the fatal battle of Maiwand.
+{}
+The campaign brought honours and promotion to many, but for me it had nothing but misfortune and disaster. I was removed from my brigade and attached to the Berkshires, with whom I served at the fatal battle of Maiwand.
 
 There I was struck on the shoulder by a Jezail bullet, which shattered the bone and grazed the subclavian artery. I should have fallen into the hands of the murderous Ghazis had it not been for the devotion and courage shown by Murray, my orderly, who threw me across a pack-horse, and succeeded in bringing me safely to the British lines.{}\
 """)
