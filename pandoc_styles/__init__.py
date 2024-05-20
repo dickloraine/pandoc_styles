ed85512bb31dfb0b036bcf88cdc9a731a723485b
dickloraine <dickloraine@gmx.net>
2024-05-20 23:36:47 +0200

    updated version

--- pandoc_styles/__init__.py
+++ pandoc_styles/__init__.py
@@ -4,3 +4,10 @@ from .flight_scripts import run_postflight_script, run_preflight_script
 from .filter import *  # pylint: disable=wildcard-import
 from .constants import *  # pylint: disable=wildcard-import
 from .main import PandocStyles
+from panflute import (  # pylint: disable=unused-import
+    Null, HorizontalRule, Space, SoftBreak, LineBreak, Str,
+    Code, BlockQuote, Note, Div, Plain, Para, Emph, Strong, Strikeout,
+    Superscript, Subscript, SmallCaps, Span, RawBlock, RawInline, Math,
+    CodeBlock, Link, Image, BulletList, OrderedList, DefinitionList,
+    LineBlock, Header, Quoted, Cite, Table, ListContainer, TableCell, Block,
+    convert_text, Element, run_filter)
